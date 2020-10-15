fc9bf7a62566f4e2e8e91fe0fcf235e44f1071c9
Mike Jolley <mike.jolley@me.com>
2020-10-15 15:48:17 +0100

    DI classes

---
+++ src/Registry/FactoryType.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Holds the FactoryType class
+ *
+ * @package MJ/PostSeries
+ */
+
+namespace MJ\PostSeries\Registry;
+
+/**
+ * Definition for the FactoryType dependency type.
+ */
+class FactoryType extends AbstractDependencyType {
+	/**
+	 * Invokes and returns the value from the stored internal callback.
+	 *
+	 * @param Container $container  An instance of the dependency injection
+	 *                              container.
+	 *
+	 * @return mixed
+	 */
+	public function get( Container $container ) {
+		return $this->resolve_value( $container );
+	}
+}
---
+++ src/Registry/SharedType.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Holds the SharedType class definition
+ *
+ * @package MJ/PostSeries
+ */
+
+namespace MJ\PostSeries\Registry;
+
+/**
+ * A definition for the SharedType dependency type.
+ */
+class SharedType extends AbstractDependencyType {
+
+	/**
+	 * Holds a cached instance of the value stored (or returned) internally.
+	 *
+	 * @var mixed
+	 */
+	private $shared_instance;
+
+	/**
+	 * Returns the internal stored and shared value after initial generation.
+	 *
+	 * @param Container $container An instance of the dependency injection
+	 *                             container.
+	 *
+	 * @return mixed
+	 */
+	public function get( Container $container ) {
+		if ( empty( $this->shared_instance ) ) {
+			$this->shared_instance = $this->resolve_value( $container );
+		}
+		return $this->shared_instance;
+	}
+}
