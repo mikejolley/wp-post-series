da1d2eee280dc535308dbb711312b1a8946dcd15
Mike Jolley <mike.jolley@me.com>
2020-10-15 15:47:50 +0100

    Build and standards

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+// Require path.
+const path = require('path');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const config = {
+	mode: 'production',
+	entry: {
+		frontend: './assets/js/frontend.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: [path.resolve(__dirname, 'assets/js')],
+				use: {
+					loader: 'babel-loader?cacheDirectory',
+					options: {
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									modules: false,
+									targets: {
+										browsers: [
+											'extends @wordpress/browserslist-config',
+										],
+									},
+								},
+							],
+						],
+						plugins: [
+							require.resolve('@babel/plugin-transform-runtime'),
+						].filter(Boolean),
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new DependencyExtractionWebpackPlugin({
+			injectPolyfill: true,
+		}),
+		new MinifyPlugin(),
+	],
+};
+const styleConfig = {
+	mode: 'production',
+	entry: {
+		'post-series': './assets/css/post-series.scss',
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: `[name]-style.js`,
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: `[name].css`,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+};
+
+module.exports = [config, styleConfig];
